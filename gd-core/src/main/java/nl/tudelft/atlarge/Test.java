aea9278b3f3baa7a5f599cf63d0a6aec18af1d4e
Chris Lemaire <c.lemaire@student.tudelft.nl>
2017-06-11 22:19:27 +0200

    Add Test main class for testing Installer functionality

---
+++ gd-core/src/main/java/nl/tudelft/atlarge/Test.java
@@ -0,0 +1,24 @@
+package nl.tudelft.atlarge;
+
+import nl.tudelft.atlarge.install.LinuxInstaller;
+import nl.tudelft.atlarge.runner.NativeLinuxRunner;
+
+import java.io.IOException;
+
+/**
+ * @author Chris Lemaire
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        NativeLinuxRunner runner = new NativeLinuxRunner();
+        LinuxInstaller installer = new LinuxInstaller(runner, "hadoop");
+
+        try {
+            installer.install("2.6.2", "$HOME/frameworks/");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
